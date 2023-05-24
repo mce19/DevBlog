613bde851d4f13a95837df8f44f87a59a3da040b
mce19 <maycolcastroesp19@gmail.com>
2023-05-23 19:55:53 -0600

    creando el header de navegacion

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -1,20 +1,41 @@
-// una funcion con row fuctions
+import Link from 'next/link'
 import Image from "next/image";
-export default function Headerpage () {
+
+const links = [{
+    label: 'Home',
+    route: '/'
+  }, {
+    label: 'About',
+    route: '/about'
+  },{
+    label: 'Footer',
+    route: '/footer'
+  }]
+
+// una funcion con row fuctions
+const Headerpage = () => {
   return (
-  <header>
-    <div className="contenedor">
+    <header >
+          <div className="contenedor">
     <Image
       src="/logo-p-svg.svg"
       width={100}
       height={40}
-      alt="Logo Andres Vargas"/>
-
-    <nav>
-
-    </nav>
-
+      alt="Andres Vargas"/>
     </div>
-  </header>
+  <nav>
+    <ul>
+    {links.map(({ label, route }) =>(
+      <li key={route}>
+       <Link href={route}>
+        {label}
+       </Link>
+      </li>
+    ))}
+    </ul>
+  </nav>
+</header>
   );
 }
+
+export default Headerpage;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Outfit } from 'next/font/google';
 import './globals.css'
-import Navegation from './components/Navegation';
 import Headerpage from './header/page';
 
 
@@ -12,7 +11,7 @@ const outfit = Outfit({
   variable: '--fuente-principal'
 });
 
-export default function RootLayout({ children, title = '', description = '' }) {
+export default function RootLayout({ children, title = 'Andres Vargas', description = '' }) {
   return (
     <html lang="es">
       <head>
@@ -22,8 +21,6 @@ export default function RootLayout({ children, title = '', description = '' }) {
       </head>
       <body className={outfit.variable}>
      < Headerpage />
-     <Navegation/>
-     <h1>Desde el layout el cual se muestra en todas las page</h1>
         {children}
 
         </body>
